b98a29915fa9852bfd9e363c1573dff1be2042ea
Hackersmate-Aditya <68347060+Hackersmate-Aditya@users.noreply.github.com>
2024-03-14 14:24:08 +0530

    Update app.py
    Added feedback and URL redirection

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ async def process_question(user_question, user_location, user_doj):
         print("After thread create")
         user_question = user_question.lower()
 
-        # a_thread = None  
+        # a_thread = None
 
         def create_new_thread():
             new_thread = client.beta.threads.create(timeout=2)
@@ -67,13 +67,13 @@ async def process_question(user_question, user_location, user_doj):
 
         run = client.beta.threads.runs.create(
             thread_id=thread.id,
-            assistant_id=assistant_id,timeout=2 
+            assistant_id=assistant_id,timeout=2
             # print("Inside run ")
         )
         print("After run")
 
 
-        while True:    
+        while True:
             try:
                 run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id, timeout=3)
                 if run.status == "completed":
@@ -93,7 +93,7 @@ async def process_question(user_question, user_location, user_doj):
                     )
                 else:
                     raise
-    
+
 
         messages = client.beta.threads.messages.list(thread_id=thread.id,timeout=3)
         latest_message = messages.data[0]
@@ -110,6 +110,39 @@ async def process_question(user_question, user_location, user_doj):
         if text.count("https") > 1:
             text = f"Here are several URLs pertinent to your inquiry. Please select the one that best matches your needs. To ensure greater accuracy in the results, please furnish additional details in your queries {text}"
 
+        opinion_text = ['We value your feedback! Share your thoughts with us at:', 'To share your opinion, please visit the following link:','Have something to say? Your opinion matters! Click here:','Express yourself and share your opinions here:','Opinions welcome! Let us know what you think at:']
+        feedback_link = "https://feedback.creatingwow.in/"
+        if user_question == 'opinion':
+            text = text.replace(text,'')
+            random_text_opinion = random.choice(opinion_text) + feedback_link
+            text = f"{random_text_opinion}"
+
+        report_text = ['To submit a report, please use the following link:','Report any issues or concerns using this link:','Help us by submitting a report through the following link:','If you need to file a report, click on the following link:','Use the following link to submit feedback or report any issues:']
+        if user_question == 'report':
+            text = text.replace(text,'')
+            random_text_report = random.choice(report_text) + feedback_link
+            text = f"{random_text_report}"
+
+        ananymous_text = ['Provide anonymous feedback by visiting the following link:','For anonymous reporting, please visit this link:','Your privacy is assured. Submit feedback anonymously:','Make suggestions anonymously by clicking on this link:']
+        if user_question == 'anonymous':
+            text = text.replace(text,'')
+            random_text_anonymous = random.choice(ananymous_text) + feedback_link
+            text = f"{random_text_anonymous}"
+
+        response_text = ['Share your response with us by visiting the following link:','Submit your response easily through this link:','Share your thoughts and responses by clicking here:','Give your feedback and response using this link:','We value your response! Respond to us by visiting:','Open the response gateway and submit your thoughts at:']
+        if user_question == 'response':
+            text = text.replace(text,'')
+            random_text_response = random.choice(response_text) + feedback_link
+            text = f"{random_text_response}"
+
+        if "https://itsupport.infobeans.com/support/solutions/folders/22222222" in text:
+            text = text.replace("https://itsupport.infobeans.com/support/solutions/folders/22222222","https://itsupport.infobeans.com/support/solutions/folders/27000104399?page=2&per_page=10")
+
+        if "https://itsupport.infobeans.com/support/solutions/folders/44444444" in text:
+            text = text.replace("https://itsupport.infobeans.com/support/solutions/folders/44444444","https://itsupport.infobeans.com/support/solutions/folders/27000104399")
+
+        if "https://itsupport.infobeans.com/support/solutions/folders/33333333" in text:
+            text = text.replace("https://itsupport.infobeans.com/support/solutions/folders/33333333","https://itsupport.infobeans.com/support/solutions/folders/27000104399?page=3&per_page=10")
 
         if "https://infobeans_admin_committee" in text:
             if user_location == "Pune":
@@ -133,7 +166,7 @@ async def process_question(user_question, user_location, user_doj):
                     text = text.replace("https://payroll.creatingwow.in/", "https://payroll.creatingwow.in/SEZINDORE")
 
         text = text.replace("https://payroll.creatingwow.in/unit_3#/", "https://payroll.creatingwow.in/unit_3")
-    
+
         gc.collect()
         return {'response': text, 'thread_id': thread.id}
 
